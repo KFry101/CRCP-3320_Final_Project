baad51e71ca3d0a581ce0526f9f3430060efc3fe
KFry101 <katiefrymire@gmail.com>
2025-10-02 10:40:06 -0500

    in class: working with bootstrap cards

--- src/app.mjs
+++ src/app.mjs
@@ -3,8 +3,13 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-
+//in class
 app.use(express.static('public'));
+
+
+app.get('/', (request, response) => {
+    response.send('Hello, World!!!!!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
