6b84ee7a2cf55c2e8659615bb462644a737ab024
KFry101 <katiefrymire@gmail.com>
2025-10-14 09:47:09 -0500

    in class

--- src/app.mjs
+++ src/app.mjs
@@ -3,13 +3,18 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-//Bootstrap and static files
-app.use(express.static('public'));
+//Bootstrap and other public files
+//app.use(express.static('public'));
 
 //Routes
 app.get('/', (request, response) => {
     response.send('Hello, World!!!');
 });
+
+app.get('/randomColor', (request, response) => {
+    response.send('About me');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
