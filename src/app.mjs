e9f18a420c9903cb92b8d759303b6d4a42b5afcd
KFry101 <katiefrymire@gmail.com>
2025-10-16 13:46:57 -0500

    Starting the customizing proccess

--- src/app.mjs
+++ src/app.mjs
@@ -4,17 +4,12 @@ const app = express();
 const port = 3000;
 
 //Bootstrap and other public files
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 //Routes
-app.get('/', (request, response) => {
-    response.send('Hello, World!!!');
-});
-
-app.get('/randomColor', (request, response) => {
+app.get('/AboutMe', (request, response) => {
     response.send('About me');
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
