2376801bb856ecab8803eb50bb7a545d0b48b37f
KFry101 <katiefrymire@gmail.com>
2025-10-16 09:51:41 -0500

    Route Parameters and querys

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import { RandomColor } from './utils/random_color.mjs';
+import { RandomNumber } from './utils/random_number.mjs';
 
 const app = express();
 const port = 3000;
@@ -16,8 +17,14 @@ app.get('/randomColor', (request, response) => {
 app.get('/api/randomColor', (request, response) => {
   const randomColor = RandomColor.getRandomHex();
   response.json({ color: randomColor });
-    
 })
+
+app.get('/api/randomNumber', (request, response) => {
+  const min = parseFloat(request.query.min);
+  const max = parseFloat(request.query.max);
+  const randomNumber = RandomNumber.getRandomFloat(min, max);
+  response.json({ number: randomNumber });
+});
 
 //Listener
 app.listen(port, () => {
