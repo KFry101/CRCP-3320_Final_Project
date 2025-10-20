35109d692a45392396728c04dcb6228fcd175105
KFry101 <katiefrymire@gmail.com>
2025-10-20 14:23:20 -0500

    starting the basic structure for other pages

--- src/app.mjs
+++ src/app.mjs
@@ -5,10 +5,17 @@ import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
 
-//Bootstrap and other public files
-app.use(express.static('public'));
+//Static Assests
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../public')));
 
 //Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, '../public', 'about.html')));
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
