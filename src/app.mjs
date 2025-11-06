5b6320f02cf408f1fa1f8a4c0bf025771733ed4c
KFry101 <katiefrymire@gmail.com>
2025-11-06 14:57:48 -0600

    Fixed routing; added ejs; started to set up the artwork pages for the sql server

--- src/app.mjs
+++ src/app.mjs
@@ -1,19 +1,71 @@
 import express from 'express';
+import { encode } from 'html-entities';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
-//Static Assests
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+/*const mysql = require('mysql'); // Will eventually be using mySql
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'your_user',
+    password: 'your_password',
+    database: 'your_database'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to MySQL database.');
+});
+*/
 
 //Routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-app.get('/about', (req, res) => res.sendFile(path.join(__dirname, '../public', 'about.html')));
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
+app.get('/artwork/:id', (req, res) => {
+  let artId = req.params.id; //ID value in Mysql table in db eventually 
+  artId = encode(artId);
+  const sqlQuery = 'SELECT * FROM arts WHERE id = ?';
+  //db will be fetch from sql art odject
+  /*
+  db.query(sqlQuery, [artId], (err, results) => {
+    if (err) {
+      // Handle error appropriately
+      console.error(err);
+      res.status(500).send('Error retrieving user data');
+      return;
+    }
+  // The results will be an array of rows. Access the first row (index 0).
+  // Ensure a row was actually found
+  if (results.length > 0) {
+    const user = results[0];
+    // Pass the single user object to the EJS template
+    res.render('artwork', { art: art });
+  } else {
+      res.status(404).send('Art not found');
+  }
+  */
+  res.render('artwork', {artName: artId});
+});
+
+app.use((request, response, next) => {
+  response.status(404).render('errors/404');
+});
 
 
 
